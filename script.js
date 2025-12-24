3b3825107623077125112889dc2ef589a19010c6
Coko <91132775+Coko7@users.noreply.github.com>
2025-12-24 11:31:01 +0100

    feat: improve hobbies secticon & clicker + implement home lab section

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
+let counterDisplayed = false;
 let clickedMeCounter = 0;
 
+let overCraftSpan;
+let profileImg;
+let introArticle;
+
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("hello there")
+  console.log("hello there");
 
-  const profileImg = document.getElementById('profile-picture');
-  const overCraftSpan = document.querySelector('.hovercraft>span');
+  overCraftSpan = document.querySelector('.hovercraft>span');
+  profileImg = document.getElementById('profile-img');
+  introArticle = document.querySelector('article.intro');
 
-  profileImg.addEventListener('click', function() {
-    clickedMeCounter++;
-    if (clickedMeCounter === 1) {
+  const articlesH3 = document.querySelectorAll('.title');
+  const copyrightSpan = document.getElementById('copyright-span');
+
+  profileImg.addEventListener('click', () => {
+    if (!counterDisplayed) {
+      counterDisplayed = true;
       overCraftSpan.classList.toggle('hidden');
+      setInterval(() => {
+        incrementClicks(1);
+      }, 1000)
     }
-    overCraftSpan.textContent = `> ${clickedMeCounter} <`;
+    incrementClicks(1);
+  })
+
+  articlesH3.forEach((h3) => {
+    h3.addEventListener('click', () => {
+      incrementClicks(5);
+    })
+  })
+
+  copyrightSpan.addEventListener('click', () => {
+    incrementClicks(15);
   })
 });
+
+function incrementClicks(amount) {
+  if (!counterDisplayed) return;
+
+  clickedMeCounter += amount;
+
+  if (clickedMeCounter > 9000) {
+    overCraftSpan.textContent = `> It's Over 9000! <`;
+  } else {
+    overCraftSpan.textContent = `> ${clickedMeCounter} <`;
+  }
+
+  // let marginTop = parseInt(window.getComputedStyle(introArticle).marginTop, 10);
+  // marginTop += amount;
+  // introArticle.style.marginTop = marginTop + 'px';
+}
