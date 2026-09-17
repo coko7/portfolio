f3adf8d1a27804b26282aac3c1e363a9aec71b35
Coko <91132775+Coko7@users.noreply.github.com>
2026-09-17 07:13:10 +0200

    chore: add WB SVG + improve TT section + styles
    - replace the static PNG screenshot of the World Building wiki with
      an SVG generated by Obsidian Extended Graph plugin (svg data assets
      also post-processed afterwards to remove labels and link infos)
    - add latest videos to Terminal Collective section (uses YouTube Data
      API with a website-restricted API key for scoping usage)
    - add Twitch channel to social section
    - tweak content and styles

--- script.js
+++ script.js
@@ -48,7 +48,41 @@ document.addEventListener("DOMContentLoaded", function () {
   copyrightSpan.addEventListener("click", () => {
     incrementClicks(15);
   });
+
+  loadLatestVideos();
 });
+
+// Feel free to steal and re-use the key *if you can* ( ͡° ͜ʖ ͡°)
+const API_KEY = 'AIzaSyBYxaee673AnhG4FdKAspVKh5TVPUs7gpc';
+// https://www.youtube.com/@TerminalCollectiveOrg
+const CHANNEL_ID = 'UCuxDrxscs0N-EmpXspDdhRg';
+
+async function loadLatestVideos() {
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=3&type=video`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const container = document.getElementById('latest-videos');
+  if (!data.items) {
+    console.error("failed to load latest videos from Terminal Collective");
+    container.innerHTML = "<b style=\"color: #F44336\">... Could not load latest YouTube videos (╯`Д´)╯︵ ┻━┻</b>"
+    return;
+  }
+
+  container.innerHTML = data.items.map(item => {
+    const videoId = item.id.videoId;
+    const thumb = item.snippet.thumbnails.medium.url;
+    const title = item.snippet.title;
+
+    return `
+      <a class="video-card" href="https://www.youtube.com/watch?v=${videoId}" target="_blank" rel="noopener">
+        <img src="${thumb}" alt="${title}" loading="lazy">
+        <p>${title}</p>
+      </a>
+    `;
+  }).join('');
+}
 
 function incrementClicks(amount) {
   if (!counterDisplayed) return;
